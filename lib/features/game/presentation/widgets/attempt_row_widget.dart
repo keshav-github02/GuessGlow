065c875a7e279e35c38623c47e02aa447a9cdfb7
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-06-21 15:36:20 +0530

    moving formward

--- lib/features/game/presentation/widgets/attempt_row_widget.dart
+++ lib/features/game/presentation/widgets/attempt_row_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guesstoget/core/theme/app_colors.dart';
+import 'package:guesstoget/features/game/presentation/widgets/letter_box_widget.dart';
 
 import '../bloc/game_bloc.dart';
 import '../bloc/game_state.dart';
@@ -12,21 +14,32 @@ class AttemptRowWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<GameBloc, GameState>(
       builder: (context, state) {
-        final word=state.word ?? '';
-        final previousAttempts=state.attempts??[];
-        final currentAttempt=state.currentAttempt??'';
-        final isCurrentIndex=attemptIndex == previousAttempts.length;
+        final word = state.word ?? '';
+        final previousAttempts = state.attempts ?? [];
+        final currentAttempt = state.currentAttempt ?? '';
+        final isCurrentAttempt = attemptIndex == previousAttempts.length;
         return Row(
-          children:List.generate(word.length, (letterIndex){
-            return Expanded(child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              margin: EdgeInsets.all(10),
+          children: List.generate(word.length, (letterIndex) {
+            var text=_getLetter(letterIndex, attemptIndex, previousAttempts, currentAttempt, isCurrentAttempt);
+            return Expanded(
+                child: LetterBoxWidget(
+              text: text,
+              boxColor: AppColors.green,
+              textColor: AppColors.surface,
             ));
           }),
         );
       },
     );
+  }
+
+  String _getLetter(int letterIndex, int attemptIndex, List<String> previousAttempts,String currentAttempt,bool isCurrentAttempt){
+    if(attemptIndex < previousAttempts.length){
+      return previousAttempts[attemptIndex][letterIndex];
+    }
+    else if(isCurrentAttempt){
+      return letterIndex<currentAttempt.length ? currentAttempt[letterIndex]:' ';
+    }
+    return ' ';
   }
 }
